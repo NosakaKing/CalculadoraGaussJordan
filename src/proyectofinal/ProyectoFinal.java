a408a4b8b8dc3845a01492dc5cb7343daf265f83
Justin <justinmoreiragarcia@gmail.com>
2023-02-24 15:51:34 -0500

    Calculadora Gauss

---
+++ src/proyectofinal/ProyectoFinal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectofinal;
+
+/**
+ *
+ * @author Raul
+ */
+public class ProyectoFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MetodoGaussJordan Gauss = new MetodoGaussJordan();
+        Gauss.setVisible(true);
+        
+    }
+    
+}
